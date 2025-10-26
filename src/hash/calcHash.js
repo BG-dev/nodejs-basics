ea4a8c3213ae4534bbc598f2bcd0c691e0b7d182
nikita.azizov <nik.azizoff2016@gmail.com>
2025-10-27 00:28:23 +0300

    feat: add implementation of calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import { createHash } from "crypto";
+import { createReadStream } from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const calculateHash = async () => {
-  // Write your code here
+  const filesDir = join(__dirname, "files");
+  const srcFilePath = join(filesDir, "fileToCalculateHashFor.txt");
+
+  const stream = createReadStream(srcFilePath);
+
+  return new Promise((resolve) => {
+    const hash = createHash("sha256");
+    stream.on("data", (data) => hash.update(data, "utf-8"));
+    stream.on("end", () => {
+      console.log(hash.digest("hex"));
+      resolve();
+    });
+  });
 };
 
 await calculateHash();
