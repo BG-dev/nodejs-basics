805fa9c1499f349f3184a5244f9d2b425d1ebf7e
nikita.azizov <nik.azizoff2016@gmail.com>
2025-10-27 00:30:10 +0300

    feat: add implementation of child processes

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,14 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { fork } from "node:child_process";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const spawnChildProcess = async (args) => {
-  // Write your code here
+  const filesDir = join(__dirname, "files");
+  const scriptFilePath = join(filesDir, "script.js");
+
+  fork(scriptFilePath, args);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["mem, wow, kek"]);
