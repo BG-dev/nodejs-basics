fd7cba7eec1f64cda847b7a70f5e1c2678741996
nikita.azizov <nik.azizoff2016@gmail.com>
2025-10-27 00:29:47 +0300

    feat: add implementation of worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,36 @@
+import { cpus } from "os";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { Worker } from "worker_threads";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const performCalculations = async () => {
-  // Write your code here
+  const workerFilePath = join(__dirname, "worker.js");
+
+  const numberCores = cpus().length;
+  let number = 10;
+  const workers = Array.from(
+    { length: numberCores },
+    () =>
+      new Promise((resolve, reject) => {
+        const worker = new Worker(workerFilePath);
+        worker.postMessage(number++);
+
+        worker.on("message", (data) => {
+          resolve(data);
+          worker.terminate();
+        });
+
+        worker.on("error", (data) => {
+          reject(data);
+          worker.terminate();
+        });
+      })
+  );
+
+  const msg = await Promise.all(workers);
+  console.log(msg);
 };
 
 await performCalculations();
