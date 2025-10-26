c1e9787c48538d2f57029e81e71c2c3f5d559dd6
nikita.azizov <nik.azizoff2016@gmail.com>
2025-10-26 20:35:48 +0300

    feat: add implementation of copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,23 @@
+import { cp } from "fs/promises";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const copy = async () => {
-  // Write your code here
+  const srcDir = join(__dirname, "files");
+  const destDir = join(__dirname, "files_copy");
+  try {
+    await cp(srcDir, destDir, {
+      recursive: true,
+      errorOnExist: true,
+      force: false,
+    });
+  } catch (error) {
+    if (error.code === "ENOENT" || error.code === "ERR_FS_CP_EEXIST") {
+      throw new Error("FS operation faile");
+    }
+  }
 };
 
 await copy();
