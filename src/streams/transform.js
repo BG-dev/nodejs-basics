4813133892e08bce935786121bab2247f2da8e08
nikita.azizov <nik.azizoff2016@gmail.com>
2025-10-27 00:28:55 +0300

    feat: add implementation of transform.js by streams

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+import { stdin, stdout } from "process";
+import { Transform } from "stream";
+import { pipeline } from "stream/promises";
+
 const transform = async () => {
-  // Write your code here
+  const transformStream = new Transform({
+    transform(data, _, cb) {
+      const result = data.toString().split("").reverse().join("");
+
+      cb(null, result + "\n");
+    },
+  });
+
+  await pipeline(stdin, transformStream, stdout);
 };
 
 await transform();
